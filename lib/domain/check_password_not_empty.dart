5686f64e383845117699fe56b23955691554f951
hunter350 <hunter350@mail.ru>
2024-10-21 21:36:40 +0300

    remove String

--- lib/domain/check_password_not_empty.dart
+++ lib/domain/check_password_not_empty.dart
@@ -1,6 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final checkPasswordProvider = StateProvider<String>(
-  // We return the default sort type, here name.
-      (ref) => '',
-);
+final checkPasswordProvider = StateProvider<String>((ref) => '');
